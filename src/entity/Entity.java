0a622ab1a25b11242e67ea55b2592f53421c6ace
Gaia <gaia.leoni.968@gmail.com>
2025-10-04 10:52:33 +0200

    Animazione

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,10 +1,14 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
     public int x, y;
     public int speed;
 
     public String direction;
+
+    public BufferedImage warrior;
 
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
 import main.KeyHandler;
@@ -27,7 +31,12 @@ public class Player extends Entity{
     }
 
     public void getPlayerImage(){
+        try {
+            warrior = ImageIO.read(getClass().getResourceAsStream("/res/player/Warrior_blue.png"));
         
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update(){
@@ -49,10 +58,40 @@ public class Player extends Entity{
         }
     }
 
+    final int spriteWidth = 192;
+    final int spriteHeight = 192;
+    int frameX = 0;
+    int frameY = 1;
+    int gameFrame = 0;
+    final int straggerFrames = 5;
+
     public void draw(Graphics2D g2){
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        //g2.setColor(Color.white);
+        //g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = warrior;
+        
+        g2.drawImage(   image, 
+                        x, //dx1
+                        y, //dy1
+                        x+spriteWidth, //dx2
+                        y+spriteHeight, //dx2
+                        frameX*spriteWidth, //sx1
+                        frameY*spriteHeight, //sy1
+                        (frameX+1)*spriteWidth, //sx2
+                        (frameY+1)*spriteHeight, //sy2
+                        null); //observer
+        
+        if (gameFrame % straggerFrames == 0){
+            if (frameX < 5){
+                frameX++;
+                System.out.println(frameX);
+            }else 
+                frameX = 0;
+        }
+
+        gameFrame++;
+        
     }
+}
     
 
-}
