c6d94f41d9f9a9f6a7b7e7ee10dc368619ad2f29
Gaia <gaia.leoni.968@gmail.com>
2026-06-12 21:46:50 +0200

    attivazione screen win

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -562,13 +562,11 @@ public class GameModel implements Serializable {
                     currentLevel ++;
                     currentLevelPowerUpCollected = false;
                     System.out.println("Level passed! Current Level: "+currentLevel);
-                    gameState = GameState.WIN;
                 } else {
                     // all levels passed 
                     updateMonkPositionForEndLevel();
                     levelCompleted = true;
                 System.out.println("Level passed! End level "+currentLevel);
-
 
                 }
             }
